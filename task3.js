6aad1a258c662e8f30bd93806bdf50337c8cf24f
kistikkkk <kistikkkk@mail.ru>
2024-12-27 05:46:22 +0300

    Добавлен файл task3.js

---
+++ task3.js
@@ -0,0 +1,7 @@
+console.log("Ребро куба a задается случайно");
+let a = Math.floor(Math.random() * 101);
+console.log("Ребро куба(a) = " + a);
+let v = Math.pow(a,3);
+console.log("Объем куба(v) = " + v);
+let s = 6*Math.pow(a,2);
+console.log("Площадь поверхности куба(s) = " + s);
