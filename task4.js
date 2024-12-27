3e0bbf65cfb8d0994143a69f88fb8e1b9ea0d876
kistikkkk <kistikkkk@mail.ru>
2024-12-27 05:47:13 +0300

    Добавлен файл task4.js

---
+++ task4.js
@@ -0,0 +1,9 @@
+console.log("Числа задаются случайно");
+let a = Math.floor(Math.random() * 101);
+console.log("a = " + a);
+let b = Math.floor(Math.random() * 101);
+console.log("b = " + b);
+let s = (a + b)/2;
+console.log("Среднее арифметическое чисел = " + s);
+let m = Math.sqrt(a*b);
+console.log("Среднее геометрическое чисел = " + m);
